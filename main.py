cd47ccb25f57c92ac70eb73960fdd2dfe7b3a7b5
heyhenry <henryyy3@hotmail.com>
2024-10-04 16:31:14 +0800

    created initial setup of setuppage.

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ class MainApp(tk.Tk):
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
+        self.geometry('1200x800')
+
         # intialise frames to an empty array
         self.frames = {}
 
@@ -24,15 +26,63 @@ class MainApp(tk.Tk):
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky='nswe')
 
+        self.show_frame(SetupPage)
+
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
 
 class SetupPage(tk.Frame):
-    pass
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        self.controller = controller
+
+        self.username_var = tk.StringVar()
+        self.password_var = tk.StringVar()
+        self.confirm_password_var = tk.StringVar()
+
+        self.create_widgets()
+
+    def create_widgets(self):
+        form_wm = tk.Frame(self)
+        form_wm.place(relx=0.5, rely=0.5, anchor='center')
+        form_wm.propagate(0)
+        form_wm.config(width=600, height=600)
+
+        setup_title = tk.Label(form_wm, text='Setup Account', font=('helvetica', 32))
+        username_subtitle = tk.Label(form_wm, font=('helvetica', 12), text='Username:', borderwidth=2)
+        username_entry = tk.Entry(form_wm, font=('helvetica', 18), textvariable=self.username_var)
+        password_subtitle = tk.Label(form_wm, text='Password:', font=('helvetica', 12), borderwidth=2)
+        password_entry = tk.Entry(form_wm, font=('helvetica', 18), textvariable=self.password_var)
+        confirm_password_subtitle = tk.Label(form_wm, font=('helvetica', 12), text='Confirm Password:', borderwidth=2)
+        confirm_password_entry = tk.Entry(form_wm, font=('helvetica', 18), textvariable=self.confirm_password_var)
+        setup_submission = tk.Button(form_wm, text='Proceed')
+
+        setup_title.place(x=160, y=50)
+        username_subtitle.place(x=170, y=130)
+        username_entry.place(x=170, y=150)
+        password_subtitle.place(x=170, y=230)
+        password_entry.place(x=170, y=250)
+        confirm_password_subtitle.place(x=170, y=330)
+        confirm_password_entry.place(x=170, y=350)
+        setup_submission.place(x=240, y=450)
+
 
 class LoginPage(tk.Frame):
-    pass
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        lbl1 = tk.Label(self, text='poop')
+        lbl1.pack()
 
 class ProfilePage(tk.Frame):
-    pass
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        lbl2 = tk.Label(self, text='wee')
+        lbl2.pack()
+
+if __name__ == "__main__":
+    app = MainApp()
+    app.mainloop()
