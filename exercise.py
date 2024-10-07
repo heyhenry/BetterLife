6c41c17a5b80b02a846459d98a62b653164b4c69
heyhenry <henryyy3@hotmail.com>
2024-10-07 13:17:10 +0800

    created workout and exercise class objects

---
+++ exercise.py
@@ -0,0 +1,6 @@
+class Exercise:
+    def __init__(self, exercise_name, set_count, rep_count, weight_used):
+        self.exercise_name = exercise_name
+        self.set_count = set_count
+        self.rep_count = rep_count
+        self.weight_used = weight_used
---
+++ workout.py
@@ -0,0 +1,5 @@
+class Workout:
+    def __init__(self, date, time_spent, exercise_list):
+        self.date = date
+        self.time_spent = time_spent
+        self.exercise_list = exercise_list
